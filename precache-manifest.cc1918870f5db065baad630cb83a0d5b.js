3d445d78c61633fded2e0ed82c6b6739b28b4465
gadenahi@gmail.com <gadenahi@gmail.com>
2020-08-27 16:05:56 -0700

    Updates

---
+++ precache-manifest.cc1918870f5db065baad630cb83a0d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff28144d702ad23ee1415651b77cd8cd",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "9831612753c071f9bcb4",
+    "url": "/js-calculator/static/css/main.531b1d57.chunk.css"
+  },
+  {
+    "revision": "b9dc456f1880176b7768",
+    "url": "/js-calculator/static/js/2.0e29f5f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-calculator/static/js/2.0e29f5f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9831612753c071f9bcb4",
+    "url": "/js-calculator/static/js/main.57edd6a0.chunk.js"
+  },
+  {
+    "revision": "ffa3baed92fe0e1b75c4",
+    "url": "/js-calculator/static/js/runtime-main.ed0fd4cc.js"
+  }
+]);
